5c01416674ac103a53fd8ef23fd07a369f4587c3
尼采般地抒情 <wztlink1013@163.com>
2022-10-07 12:32:51 +0800

    feat: 添加live2d加密文件夹

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/encrypted/encrypted_test', '/🥟 Some test files/Protected route'],
+  protectedRoutes: ['/live2d', '/encrypted/encrypted_test', '/🥟 Some test files/Protected route'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:wztlink1013@163.com',
